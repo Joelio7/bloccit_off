08169d15382c88fc52676979aa2ec53596a2a855
Your Name <joe.scalera@gmail.com>
2015-11-10 22:27:36 -0800

    item checkoff and time in words

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,17 +3,36 @@ class ItemsController < ApplicationController
 
   def create
     @user = current_user
+    @new_item = Item.new
 
     if @item = @user.items.create(item_params)
-       flash[:notice] = "assignment created"
+       flash[:notice] = "item created"
     else
       flash[:error] = "Error saving item. Try again."
     end
 
-    redirect_to current_user
+    respond_to do |format|
+      format.html {redirect_to request.referer}
+      format.js
+    end
   end
 
-def item_params
-  params.require(:item).permit(:name)
-end
+  def destroy
+    @user = current_user
+    @item = Item.find(params[:id])
+
+    if @item.destroy
+      flash[:notice] = "\"#{@item.name}\" was deleted successfully."
+    else
+      flash[:error] = "There was an error"
+    end
+    respond_to do |format|
+      format.html {redirect_to request.referer}
+      format.js
+    end
+  end
+
+  def item_params
+    params.require(:item).permit(:name)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,7 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    @user = current_user
     @items = @user.items.order(created_at: :desc)
     @new_item = Item.new
-
-
   end
-
-
-
 end
